3870b316401469b38d2306857479bdf8031267c6
Miguel Aréjula Aísa <arejula10@gmail.com>
2025-03-26 12:18:55 +0100

    Refactor routes handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { routes } from './utils/constants';
-import ByronHub from './menu/ByronHub';
-import LogIn from './logging/LogIn';
+import RouterPrincipal from './Router/RouterPrincipal';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* home page */}
-        <Route path={routes.ByronHub} element={<ByronHub />} />
-
-        {/* login page */}
-        <Route path={routes.LogIn} element={<LogIn />} />
-      </Routes>
-    </Router>
+    <RouterPrincipal />
   );
 }
 
---
+++ src/Router/RouterPrincipal.jsx
@@ -0,0 +1,45 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import ByronHub from '../Pages/ByronHub';
+import LoginPage from '../Pages/LogIn';
+import { NotFound } from '../Pages/NotFound';
+
+const RouterPrincipal = () => {
+
+  const PrivateRoute = ({ children }) => {
+    return !localStorage.getItem('token') ? <Navigate to="/login" /> : children;
+
+  };
+ 
+  const AdminRoute = ({ children }) => {
+    //se obtiene del JWT el rol del usuario
+    const auth = JSON.parse(localStorage.getItem('esAdmin'));
+    if(!auth){
+      return <Navigate to="/login" />
+    }
+
+    return !localStorage.getItem('token') ?
+      <Navigate to="/login" />
+    :
+    auth.rol == "usuario" ? <Navigate to="/login" /> : children;
+  }
+
+  return (
+        <Router>
+          <Routes>
+            <Route path="/" element={<ByronHub/>} />
+            <Route path="/login" element={<LoginPage />} />
+
+            {/* Example of private and admin routes */}
+            {/* <Route path="/" element={<PrivateRoute><Home/></PrivateRoute>} /> */}
+            
+            <Route path="*" element={<NotFound />} />
+
+            {/* Add more routes as needed */}
+
+
+          </Routes>
+        </Router>
+  );
+}
+
+export default RouterPrincipal;
