9552f885bcb5892a44e415a54c36e03c10f6168b
Miguel Aréjula Aísa <arejula10@gmail.com>
2025-05-05 20:48:21 +0200

    update default API URL in Url constant to include '/api'

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,2 @@
-export const Url = import.meta.env.VITE_URL || 'http://localhost:3000';
+export const Url = import.meta.env.VITE_URL || 'http://localhost:3000/api';
 export const UrlPyGeoApi = import.meta.env.VITE_PY_GEO_API || 'http://localhost:5001';
